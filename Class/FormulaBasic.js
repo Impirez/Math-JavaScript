ac8ccf9bea038b60495e7aea0c69b69989afd4c8
Cesar Espinoza <ceazoffi@gmail.com>
2022-12-27 20:59:48 -0500

    reducimos lineas de codigo

--- Class/FormulaBasic.js
+++ Class/FormulaBasic.js
@@ -3,26 +3,28 @@ console.group('Cuadrado') ///Console.group('') y Console.groupEnd('') Lo que hac
 const ladosCuadrado = 5
 const perimetroCuadrado = ladosCuadrado * 4;
 const areaCuadrado = ladosCuadrado ** 2;
-
     // Para hallar arega es base * altura / 2
     //Para hallar perimetro: suma de todos los lados
-
     console.log({
         ladosCuadrado,
         perimetroCuadrado,
         areaCuadrado
     })
-
     //Aplicamos la formula en una funcion que necesita parametro
-
 function calcularCuadrado(Clado){
     return{
         area: Clado ** 2,
         perimetro: Clado * Clado
     }
 }
-
 console.groupEnd('Cuadrado')
+
+
+
+
+
+
+
 console.group('Triangulo');
 
 const ladosTriangulo1 =  6;
@@ -31,12 +33,8 @@ const ladoBaseTriangulo = 4;
 const alturaTriangulo = 5.5;
 const perimetroTriangulo = ( ladosTriangulo1 + ladosTriangulo2 + ladoBaseTriangulo)
 const areaTriangulo =  (ladoBaseTriangulo * alturaTriangulo) / 2;
-
     // Para hallar arega es base * altura / 2
     //Para hallar perimetro: suma de todos los lados
-
-
-
     console.log({
         ladosTriangulo1,
         ladosTriangulo2,
@@ -45,7 +43,6 @@ const areaTriangulo =  (ladoBaseTriangulo * alturaTriangulo) / 2;
         perimetroTriangulo,
         areaTriangulo
     })
-
     //Aplicamos la formula en funcion que necesita parametro
 
 function calcularTriangulo(Tlado1, Tlado2, Tbase, Taltura){
@@ -54,5 +51,4 @@ function calcularTriangulo(Tlado1, Tlado2, Tbase, Taltura){
         perimetro: (Tbase * Taltura) / 2
     }
 }
-
 console.groupEnd('Triangulo');
